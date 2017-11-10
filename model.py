5ba9dc09ba2e54fb1eaded145e1b56a8fab5f24f
Moiz <progrmoiz@gmail.com>
2017-11-10 21:54:50 +0500

    Delete model.py

--- model.py
+++
@@ -1,8 +0,0 @@
-from google.appengine.ext import ndb
-
-class WikiData(ndb.Model):
-    subject = ndb.StringProperty()
-    content = ndb.TextProperty()
-    created = ndb.DateTimeProperty(auto_now_add=True)
-    last_modified = ndb.DateTimeProperty(auto_now=True)
-    history = ndb.PickleProperty(compressed=True)
