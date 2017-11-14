09398de12df40601e383274e943cae1b5b14bc7e
Moiz <progrmoiz@gmail.com>
2017-11-14 08:36:31 +0500

    Added model.wiki_history.py

---
+++ model/wiki_history.py
@@ -0,0 +1,33 @@
+from google.appengine.ext import ndb
+
+from wiki import Wiki
+
+
+def wikihistory_key(name='default'):
+    return ndb.Key('wikis_history', name)
+
+
+class WikiHistory(ndb.Model):
+    """WikiHistory datastore
+
+    A place where recent wiki stores
+    """
+    url = ndb.StringProperty(required=True)
+    history = ndb.StructuredProperty(Wiki, repeated=True)
+
+    @classmethod
+    def by_path(cls, path):
+        w = WikiHistory.query(Wiki.url == path).get()
+        return w
+
+    @classmethod
+    def create_wiki_history(cls, wiki):
+        return WikiHistory(parent=wikihistory_key(),
+                           url=wiki.url,
+                           history=[wiki])
+
+    def add_wiki_history(self, wiki):
+        self.history.append(wiki)
+
+    def get_wiki(self, index):
+        return self.history[index]
